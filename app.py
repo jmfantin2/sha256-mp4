6c3b206fe8ab5b3661f674b7d274d0791e26e275
JM Fantin <joao.fantin@acad.pucrs.br>
2021-11-01 02:07:44 -0300

    essentials

---
+++ app.py
@@ -0,0 +1,17 @@
+import sys, os
+from Crypto.Hash import SHA256
+
+# pegar titulo do arquivo de video e tamanho dos blocos para quebrar (1024)
+# chamar getHash pra calcular o hash de bloco em bloco
+def run():
+  print('work in progress')
+
+# usar path.getsize pegar o tamanho do video
+# quebrar em blocos de 1024B 
+# cuidar o ultimo bloco que provavelmente nao tem size 1024
+# mostrar resultado em hexa
+def getHash(video,block):
+  print('work in progress')
+  
+if __name__ == "__main__":
+    run()
